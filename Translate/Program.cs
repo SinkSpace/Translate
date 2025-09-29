babf6b47ee1475be38fc9e900472dfd2a4733f28
SinkSpace <vtamaichuk@yandex.ru>
2025-09-29 16:39:03 +0500

    Добавьте файлы проекта.

---
+++ Translate/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyApplication
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int result;
+            int number;
+            string input;
+
+            Console.WriteLine("Введите число: ");
+            input = Console.ReadLine();
+            number = int.Parse(input);
+            
+        }
+    }
+}
